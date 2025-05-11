9fbc95fc14452f851139262abf0b536534167200
egnica <46502133+egnica@users.noreply.github.com>
2025-05-11 10:17:47 -0500

    dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,21 @@
 "use client";
-
+import styles from "../page.module.css";
 import { useEffect, useState } from "react";
 import { supabase } from "../../supabase/supabaseClient";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
+  const [serviceS, setService] = useState(null);
 
   useEffect(() => {
+    // This grabs user from local storage
     const getUser = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
       setUser(user);
+      console.log(user);
     };
 
     getUser();
@@ -19,8 +23,38 @@ export default function Dashboard() {
 
   return (
     <main>
-      <h1>Dashboard</h1>
+      <h1>Service Select</h1>
       {user ? <p>Welcome, {user.email}</p> : <p>Loading user info...</p>}
+      <div className={styles.btnContain}>
+        <div className={styles.serviceBtn}>
+          Personal <br /> Transport
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Item <br />
+          Delivery
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Light
+          <br /> Moving
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Grocery <br />
+          Shopping
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Food <br />
+          Pickup
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Prescription <br />
+          Pickup
+          
+        </div>
+        <div onClick={() => setService(aa)} className={styles.serviceBtn}>
+          Custom <br />
+          Job
+        </div>
+      </div>
     </main>
   );
 }
